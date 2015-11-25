1f9f8c876d8e06dd85f895cdb986677da1888a9e
mlgross <mlgross@Siberia>
2015-11-25 19:58:31 -0200

    carrodao updated

--- src/java/br/edu/ifsul/dao/CarroDAO.java
+++ src/java/br/edu/ifsul/dao/CarroDAO.java
@@ -1,9 +1,55 @@
 package br.edu.ifsul.dao;
 
+import br.edu.ifsul.modelo.Carro;
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 /**
  *
  * @author mlgross 
  */
-public class CarroDAO {
+public class CarroDAO implements Serializable{
     
+    @PersistenceContext(unitName = "Revenda-WebPU")
+    private EntityManager em;
+    private List<Carro> listarTodos;
+
+    public CarroDAO() {
+    }
+
+    public void persist(Carro objeto) throws Exception {
+        em.persist(objeto);        
+    }
+    
+    public void merge(Carro objeto) throws Exception {
+        em.merge(objeto);
+    }
+    
+    public void remove(Carro objeto) throws Exception{
+        objeto = em.merge(objeto);
+        em.remove(objeto);
+    }
+    
+    public Carro getObjectById(Integer id) throws Exception {
+        return em.find(Carro.class, id);
+    }
+
+    public EntityManager getEm() {
+        return em;
+    }
+
+    public void setEm(EntityManager em) {
+        this.em = em;
+    }
+
+    public List<Carro> getListarTodos() {
+        return em.createQuery("from Carro order by renavan").getResultList();
+    }
+
+    public void setListarTodos(List<Carro> listarTodos) {
+        this.listarTodos = listarTodos;
+    }
+
 }
